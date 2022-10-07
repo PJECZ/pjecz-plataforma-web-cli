b0eefc86ffdace74360ba49628151d7ce3d4cf5b
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-10-07 14:40:16 -0500

    Guardar inv equipos

--- pjecz_plataforma_web_cli/inv_equipos/app.py
+++ pjecz_plataforma_web_cli/inv_equipos/app.py
@@ -1,6 +1,7 @@
 """
 CLI Inv Equipos App
 """
+import csv
 from datetime import datetime
 
 import rich
@@ -21,6 +22,7 @@ def consultar(
     creado_hasta: str = None,
     fecha_fabricacion_desde: str = None,
     fecha_fabricacion_hasta: str = None,
+    guardar: bool = False,
     inv_custodia_id: int = None,
     inv_modelo_id: int = None,
     inv_red_id: int = None,
@@ -32,6 +34,8 @@ def consultar(
 ):
     """Consultar equipos"""
     rich.print("Consultar equipos...")
+
+    # Solicitar datos
     try:
         respuesta = get_inv_equipos(
             creado=creado,
@@ -51,8 +55,42 @@ def consultar(
     except CLIAnyError as error:
         typer.secho(str(error), fg=typer.colors.RED)
         raise typer.Exit()
+
+    # Encabezados
+    encabezados = ["ID", "Creado", "Custodia", "Marca", "Modelo", "Red", "F. Fab.", "No. S.", "No. I.", "Tipo", "D. IP", "M. A."]
+
+    # Guardar datos en un archivo CSV
+    if guardar:
+        fecha_hora = datetime.now().strftime("%Y%m%d%H%M%S")
+        nombre_archivo_csv = f"inv_equipos_{fecha_hora}.csv"
+        with open(nombre_archivo_csv, "w", encoding="utf-8") as archivo:
+            escritor = csv.writer(archivo)
+            escritor.writerow(encabezados)
+            for registro in respuesta["items"]:
+                creado = datetime.strptime(registro["creado"], "%Y-%m-%dT%H:%M:%S.%f")
+                escritor.writerow(
+                    [
+                        registro["id"],
+                        creado.strftime("%Y-%m-%d %H:%M:%S"),
+                        registro["inv_custodia_id"],
+                        registro["inv_marca_nombre"],
+                        registro["inv_modelo_descripcion"],
+                        registro["inv_red_nombre"],
+                        registro["fecha_fabricacion"],
+                        registro["numero_serie"],
+                        "" if registro["numero_inventario"] is None else str(registro["numero_inventario"]),
+                        registro["tipo"],
+                        registro["direccion_ip"],
+                        registro["direccion_mac"],
+                    ]
+                )
+        rich.print(f"Datos guardados en el archivo {nombre_archivo_csv}")
+
+    # Mostrar la tabla
     console = rich.console.Console()
-    table = rich.table.Table("ID", "Creado", "Custodia", "Marca", "Modelo", "Red", "F. Fab.", "No. S.", "No. I.", "Tipo", "D. IP", "M. A.")
+    table = rich.table.Table()
+    for enca in encabezados:
+        table.add_column(enca)
     for registro in respuesta["items"]:
         creado = datetime.strptime(registro["creado"], "%Y-%m-%dT%H:%M:%S.%f")
         table.add_row(
@@ -70,4 +108,6 @@ def consultar(
             registro["direccion_mac"],
         )
     console.print(table)
+
+    # Mostrar el total
     rich.print(f"Total: [green]{respuesta['total']}[/green] equipos")
--- pjecz_plataforma_web_cli/oficinas/app.py
+++ pjecz_plataforma_web_cli/oficinas/app.py
@@ -17,7 +17,6 @@ def consultar(
     distrito_id: int = None,
     domicilio_id: int = None,
     limit: int = LIMIT,
-    puede_agendar_citas: bool = True,
     offset: int = 0,
 ):
     """Consultar oficinas"""
@@ -27,21 +26,19 @@ def consultar(
             distrito_id=distrito_id,
             domicilio_id=domicilio_id,
             limit=limit,
-            puede_agendar_citas=puede_agendar_citas,
             offset=offset,
         )
     except CLIAnyError as error:
         typer.secho(str(error), fg=typer.colors.RED)
         raise typer.Exit()
     console = rich.console.Console()
-    table = rich.table.Table("ID", "Clave", "Distrito", "Descripcion", "P.A.C.", "Apertura", "Cierre", "L.P.")
+    table = rich.table.Table("ID", "Clave", "Distrito", "Descripcion", "Apertura", "Cierre", "L.P.")
     for registro in respuesta["items"]:
         table.add_row(
             str(registro["id"]),
             registro["clave"],
             registro["distrito_nombre_corto"],
             registro["descripcion_corta"],
-            "SI" if bool(registro["puede_agendar_citas"]) else "",
             registro["apertura"],
             registro["cierre"],
             str(registro["limite_personas"]),
